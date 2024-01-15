99e8dfc8c6db5282fad28b5dd13c9e5001a9fac9
alishanali <136475753+alishanFlynaut@users.noreply.github.com>
2024-01-16 00:06:25 +0530

    portfolio created

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header id="home">
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Zero Degree Coder</h1>
+        <h5 className="text-light">Full-stack Developer</h5>
+        <CTA />
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+        <HeaderSocials />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
